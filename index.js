a008991b669649e89ada88bfc395842f80305005
Seppi <schuchmannsebastian@gmail.com>
2019-08-25 20:28:46 +0200

    changed wtf

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('WTF').onclick = (event)=>{
+    document.getElementsByClassName('Header')[0].innerHTML = '<h1>A Creative Technologist is a grown-up child,  who compensates his lack of toys with advanced technology. </h1>';
+ document.getElementsByClassName('Header')[0].style.cssText =  'background: url(https://media.giphy.com/media/4fHmWVnUmKTNm/giphy.gif);background-size: contain;background-position: top left;-webkit-background-clip: text;color: transparent;filter:hue-rotate(179deg) saturate(2) contrast(2);';
+
+};
+
